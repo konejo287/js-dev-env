4fe03dab0fcb9c6ba95873274b2bb036e73c68f5
Mario arcos <mario.arcos.p@gmail.com>
2019-04-20 15:41:59 -0500

    updated dependencys

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -3,6 +3,8 @@ var path = require('path');    //ES5 import fashion
 var open = require('open');
 import compression from 'compression';
 
+/* eslint-disable no-console */
+
 const port = 3000;
 const app = express();
 
